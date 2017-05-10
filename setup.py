48522a1d9bb9ff2b729b0c34c32d288b0518c633
Joseph Hunkeler <jhunkeler@gmail.com>
2017-05-10 15:07:09 -0400

    Implement RELIC

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 import os
+import sys
 from setuptools import setup, find_packages
-from purge_path.extern.version import get_git_version
+sys.path.insert(1, 'relic')
+
+import relic.release
+from setuptools import setup
+
+NAME = 'purge_path'
+version = relic.release.get_info()
+relic.release.write_template(version, NAME)
 
 entry_points = {}
 package_data = {}
@@ -10,29 +18,16 @@ entry_points['console_scripts'] = [
 ]
 
 package_data[''] = ['*.txt', '*.md']
-version_py = os.path.join('purge_path', 'version.py')
 
-#Omit git hash and let setuptools add a valid build number
-git_version = get_git_version()
-if git_version.rfind('-') != -1:
-    git_version = git_version[:git_version.rfind('-')]
-
-
-with open(version_py, 'w+') as version_data:
-    version_data.write('__version__ = "{0}"\n'.format(git_version))
-
-NAME = 'purge_path'
-VERSION = git_version
 
 setup(
     name=NAME,
-    version=VERSION,
+    version=version.pep386,
     description='A small PATH manipulator',
-    provides=[NAME],
     author='Joseph Hunkeler',
     author_email='jhunk@stsci.edu',
     license='BSD',
-    url='http://bitbucket.org/jhunkeler/purge_path',
+    url='http://github.com/jhunkeler/purge_path',
     download_url='',
     use_2to3=True,
     packages=find_packages(),
