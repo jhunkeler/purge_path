87d8fe24daaf5750274c7fa464cedf6c166a3387
Joseph Hunkeler <jhunkeler@gmail.com>
2017-05-10 14:45:12 -0400

    Fix improper behavior if multiple arguments are issued

--- purge_path/purge_path.py
+++ purge_path/purge_path.py
@@ -1,20 +1,35 @@
 #!/usr/bin/env python
+from __future__ import print_function
 import os
 import sys
- 
+
+
+def purge(path, pattern):
+    if not isinstance(path, list):
+        path = path.split(':')
+
+    path_new = ''
+    for rec in path:
+        if rec == pattern:
+            # ignore pattern
+            continue
+        path_new += rec + ':'
+
+    return path_new[:-1]
+
 
 def main():
-    ARGS = sys.argv[1:]
-    PATH = os.environ['PATH']
-    PATH_NEW = []
- 
-    for arg in ARGS:
-        for path in PATH.split(':'):
-            if arg in path:
-                continue
-            PATH_NEW.append(path)   
- 
-    print("{0}".format(":".join(PATH_NEW)))
+    args = sys.argv[1:]
+    path_orig = os.environ['PATH']
+    path_new = path_orig
+
+    for arg in args:
+        path_new = purge(path_new, arg)
+
+    if path_new:
+        print(path_new)
+    else:
+        print(path_orig)
 
 
 if __name__ in '__main__':
